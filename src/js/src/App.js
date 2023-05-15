ac3a13f3ed607910bbee58eaebb31ef9385cda08
vutiendat3601 <vutien.dat.3601@gmail.com>
2023-05-16 03:05:41 +0700

    add and test Table component of Ant Design

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { Table } from "antd";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getAllStudents } from "./client.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const columns = [
+  {
+    title: "Student ID",
+    dataIndex: "studentId",
+    key: "studentId",
+  },
+  {
+    title: "First Name",
+    dataIndex: "firstName",
+    key: "firstName",
+  },
+  {
+    title: "Last Name",
+    dataIndex: "lastName",
+    key: "lastName",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Gender",
+    dataIndex: "gender",
+    key: "gender",
+  },
+];
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  const fetchStudents = () => {
+    getAllStudents()
+      .then((resp) => resp.json())
+      .then((students) => {
+        setStudents(students);
+      });
+  };
+
+  if (students && students.length) {
+    return (
+      <Table columns={columns} dataSource={students} rowKey={"studentId"} />
+    );
+  }
+  return <h1>Spring Boot with React</h1>;
+};
 
 export default App;
---
+++ src/js/src/client.js
@@ -0,0 +1,3 @@
+const getAllStudents = () => fetch('/students');
+
+export { getAllStudents };
