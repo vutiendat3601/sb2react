dafc08cc1a01fb98341545fe378ce241e2f5a47f
vutiendat3601 <vutien.dat.3601@gmail.com>
2023-05-16 01:16:23 +0700

    initial commit

---
+++ src/main/java/com/datvutech/sb2react/Sb2reactApplication.java
@@ -0,0 +1,13 @@
+package com.datvutech.sb2react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb2reactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb2reactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datvutech/sb2react/Sb2reactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datvutech.sb2react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb2reactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
